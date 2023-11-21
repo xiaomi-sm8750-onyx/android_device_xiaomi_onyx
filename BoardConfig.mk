0059dee468ac5840594c918287f3aa4d7d3841c4
Peter Cai <peter@typeblog.net>
2023-11-21 16:20:18 +0300

    onyx: Panic to recovery, not bootloader
    Signed-off-by: SharmagRit <sharmagritstuff@gmail.com>
    Change-Id: Ied2f7ff250504eec913b5485e75a9f65

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,8 @@ BOARD_KERNEL_CMDLINE := \
     mtdoops.fingerprint=$(LINEAGE_VERSION) \
     bootmonitor.fingerprint=$(LINEAGE_VERSION)
 
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
