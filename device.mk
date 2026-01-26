dff0b137b06a8c6ae9a325d4707d5aae658711e9
LuK1337 <priv.luk@gmail.com>
2026-01-26 23:28:20 +0100

    onyx: Build qti-audio-types-aidl-V1-ndk
    E AHAL_Service_QTI: Failed to dlopen libaudiocorehal.qti.so: dlopen failed: library "qti-audio-types-aidl-V1-ndk.so" not found: needed by /vendor/lib64/hw/libaudiocorehal.qti.so in namespace (default)
    
    Change-Id: I7834da183886f3453742935cf68490a63580813e

--- device.mk
+++ device.mk
@@ -88,7 +88,8 @@ PRODUCT_PACKAGES += \
     libaudioaidlcommon.vendor \
     libaudioutils_shim \
     libmediautils_vendor.vendor \
-    libmemunreachable.vendor
+    libmemunreachable.vendor \
+    qti-audio-types-aidl-V1-ndk.vendor
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8750/audio/primary-hal
 
