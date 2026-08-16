8b01025ebcd4e0fd6bf21d79f3bf7f69c8c4c18d
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-16 06:52:24 +0000

    onyx: Drop WFD blobs
    * These are broken on 17, not easily shim-able, and no 17 devices
      are, or will soon ship on stock.
    * Therefore, drop these until/if 17 blobs that work pop up, and even
      then I'd rather it just be a common repo we all inherit anyway.
    
    Change-Id: I64a4d14f2b624399d824d2c973ee8274affc4e1a

--- extract-files.py
+++ extract-files.py
@@ -257,14 +257,6 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/vintf/manifest/vendor.qti.qesdsys.service.xml': blob_fixup()
         .regex_replace(r'(?s)^.*?(?=<manifest)', ''),
 
-    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
-        .add_needed('libgui_shim.so'),
-
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libbinder_shim.so')
-        .add_needed('libinput_shim.so')
-        .remove_needed('android.hidl.base@1.0.so'),
-
     'vendor/bin/system_dlkm_modprobe.sh': blob_fixup()
         .regex_replace(
             r'for kernel_ver_dir in \$\{system_dlkm_dir\}/\*;',
